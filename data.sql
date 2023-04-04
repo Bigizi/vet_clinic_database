c4acdc9aed1df429ac6805f423b6fc13fa2d7764
Bigizi <bigizinduwayo@gmail.com>
2023-04-04 13:08:53 +0200

    Add all queries requested and inserting data

---
+++ data.sql
@@ -0,0 +1,10 @@
+/* Populate database with sample data. */
+
+INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
+    VALUES (1, 'Agumon', '03-02-2020', 0, 'true', 10.23);
+INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
+    VALUES (2, 'Gabumon', '11-15-2018', 2, 'true', 8);
+INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
+    VALUES (3, 'Pikachu', '01-07-2021', 1, 'false', 15.04);
+INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
+    VALUES (4, 'Devimon', '05-12-2017', 5, 'true', 11);
---
+++ schema.sql
@@ -0,0 +1,13 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE DATABASE vet_clinic
+	
+	create table animals
+	(
+		id int PRIMARY KEY NOT NULL,
+		name varchar(50),
+		date_of_birth date,
+		escaped_attempts int,
+		neutered boolean,
+		weight_kg decimal
+	)
